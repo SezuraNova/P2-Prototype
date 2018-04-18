4556bc905c8afff375734e9d42dcd9c453e0f503
Guest User <Guest@wg401-e-01-03.mddn.vuw.ac.nz>
2018-04-18 17:01:06 +1200

    Implemented File checking and appending

--- yourscript.php
+++ yourscript.php
@@ -33,47 +33,72 @@ rewind($myfile);
 
 
 //Answer Analysis
-$A = 0;
+$Score = 0;
 
 while(!feof($myfile)) {
   $value = fgets($myfile);
   if (strcmp($value, "VP\n")==0) {
-  	$A = $A + 3;
+  	$Score = $Score + 3;
   } else if (strcmp($value, "P\n")==0) {
-  	$A = $A + 1;
+  	$Score = $Score + 1;
   } else if (strcmp($value, "N\n")==0) {
-  	$A = $A - 1;
+  	$Score = $Score - 1;
   } else if (strcmp($value, "VN\n")==0) {
-  	$A = $A - 3;
+  	$Score = $Score - 3;
   }   
 }
 fclose($myfile);
 
 echo $_GET["Issue"] . "<br>";
 
-//echo "A = " . $A . "<br>";
+//echo "A = " . $Score . "<br>";
 
 //Creates/(adds to) a user's profile.txt 
-$myprofile = fopen($_GET["name"] . " Profile.txt", "a+") or die("Unable to open file!");
-if ($A==15){
-	$Stance = "You have an extremely positive Stance to issue " . $_GET["Issue"];
-} else if ($A>=10){
-	$Stance = "You have a very positive Stance to issue " . $_GET["Issue"];
-} else if ($A>=5){
-	$Stance = "You have a positive Stance to issue " . $_GET["Issue"];
-} else if ($A>=-4){
-	$Stance = "You have a neutral Stance to issue " . $_GET["Issue"];
-} else if ($A>=-9){
-	$Stance = "You have a negative Stance to issue " . $_GET["Issue"];
-} else if ($A>=-14){
-	$Stance = "You have a very negative Stance to issue " . $_GET["Issue"];
-} else {
-	$Stance = "You have an extremely negative Stance to issue " . $_GET["Issue"];
+$profilecheck = ($_GET["name"] . " Profile.txt");
+$IssueDone = false;
+if (file_exists($profilecheck)){
+  // echo "<br>THIS FILE EXISTS<br><br>";
+  $myprofile = fopen($profilecheck, "a+") or die("Unable to open file!");
+  while(!feof($myprofile)) {
+    $IssueCheck = fgets($myprofile);
+    if (strcmp($IssueCheck, $_GET["Issue"] . "\n")==0) {
+    $IssueDone = true;
+    }
+  }
+  if ($IssueDone){
+    // echo "<br>" . $_GET["Issue"] . " " . $IssueDone ? 'true' : 'false' . "<br>";
+  } else {
+    AddToProfile($myprofile);
+  }
 }
+  else {
+  $myprofile = fopen($profilecheck, "w+") or die("Unable to open file!");
+  AddToProfile($myprofile);
+}
+echo "<br>" . file_get_contents($_GET["name"] . " Profile.txt") . "<br>";
 
-fwrite($myprofile, $Stance);
-echo file_get_contents($_GET["name"] . " Profile.txt") . "<br>";
-fclose($myprofile);
+
+function AddToProfile($File){
+  global $Score;
+  if ($Score==15){
+    $Stance = "You have an extremely positive Stance to issue " . $_GET["Issue"];
+  } else if ($Score>=10){
+    $Stance = "You have a very positive Stance to issue " . $_GET["Issue"];
+  } else if ($Score>=5){
+    $Stance = "You have a positive Stance to issue " . $_GET["Issue"];
+  } else if ($Score>=-4){
+    $Stance = "You have a neutral Stance to issue " . $_GET["Issue"];
+  } else if ($Score>=-9){
+    $Stance = "You have a negative Stance to issue " . $_GET["Issue"];
+  } else if ($Score>=-14){
+    $Stance = "You have a very negative Stance to issue " . $_GET["Issue"];
+  } else {
+    $Stance = "You have an extremely negative Stance to issue " . $_GET["Issue"];
+  }
+  echo "<br>" . $Stance . "<br>";
+  fwrite($File, $_GET["Issue"] . "\n" . $Score . "\n");
+  fclose($File);
+}
 
 ?>
 
