0d1f64b88a385218dc835f6e8666a4b4691e781e
Sezura <33705559+Sezura@users.noreply.github.com>
2018-04-26 14:50:36 +1200

    Refactured for multiple quizzes added html and css to multiple php pages

---
+++ QuizB.php
@@ -0,0 +1,147 @@
+<html lang="en"> 
+<head>
+
+    <title>The Anti-Twitter Project</title> 
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+    <meta name="description" content="This webpage explains the problem with the Attention Economy, how it effects our political landscape and some potential tech-based solutions" /> 
+    <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css" />
+
+    <link rel="stylesheet" href="css/main.css" /> 
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+</head>
+
+<body>
+    
+    <section>
+      
+        <header>
+            
+            <h3>
+                The Anti-Twitter Project
+            </h3>
+
+        </header>
+    </section>
+
+    <section class="hero">
+     <div class="scrim">
+        <h2></h2>
+    </div>
+</section>
+
+	Welcome <?php echo $_GET["name"]; ?> <br>
+
+<form action="yourscript.php" method="get">
+
+       	<input type="hidden" name="name" value = <?php$_GET["name"]?>>
+        <!-- E-mail: <input type="text" name="email"><br> -->
+
+        <input type="hidden" name= "Issue" value = "B">
+
+        <p class="question">1. How much do you agree with B1?</p>        
+
+        <input type="radio" id="B1VP" name="Q1" value="VP">
+        <label for="B1VP">Agree a lot</label><br>
+
+        <input type="radio" id="B1P" name="Q1" value="P">
+        <label for="B1P">Agree a little</label><br>   
+
+        <input type="radio" id="B1U" name="Q1" value="U">
+        <label for="B1U">Undecided</label><br>    
+
+        <input type="radio" id="B1N" name="Q1" value="N">
+        <label for="B1N">Disagree a little</label><br>
+
+        <input type="radio" id="B1VN" name="Q1" value="VN">
+        <label for="B1VN">Disagree a lot</label><br>
+
+        <p class="question">2. How much do you agree with B2?</p>        
+
+        <input type="radio" id="B2VP" name="Q2" value="VP">
+        <label for="B2VP">Agree a lot</label><br>
+
+        <input type="radio" id="B2P" name="Q2" value="P">
+        <label for="B2P">Agree a little</label><br>   
+
+        <input type="radio" id="B2U" name="Q2" value="U">
+        <label for="B2U">Undecided</label><br>    
+
+        <input type="radio" id="B2N" name="Q2" value="N">
+        <label for="B2N">Disagree a little</label><br>
+
+        <input type="radio" id="B2VN" name="Q2" value="VN">
+        <label for="B2VN">Disagree a lot</label><br>
+
+        <p class="question">3. How much do you agree with B3?</p>        
+
+        <input type="radio" id="B3VP" name="Q3" value="VP">
+        <label for="B3VP">Agree a lot</label><br>
+
+        <input type="radio" id="B3P" name="Q3" value="P">
+        <label for="B3P">Agree a little</label><br>   
+
+        <input type="radio" id="B3U" name="Q3" value="U">
+        <label for="B3U">Undecided</label><br>    
+
+        <input type="radio" id="B3N" name="Q3" value="N">
+        <label for="B3N">Disagree a little</label><br>
+
+        <input type="radio" id="B3VN" name="Q3" value="VN">
+        <label for="B3VN">Disagree a lot</label><br>
+
+        <p class="question">4. How much do you agree with B4?</p>        
+
+        <input type="radio" id="B4VP" name="Q4" value="VP">
+        <label for="B4VP">Agree a lot</label><br>
+
+        <input type="radio" id="B4P" name="Q4" value="P">
+        <label for="B4P">Agree a little</label><br>   
+
+        <input type="radio" id="B4U" name="Q4" value="U">
+        <label for="B4U">Undecided</label><br>    
+
+        <input type="radio" id="B4N" name="Q4" value="N">
+        <label for="B4N">Disagree a little</label><br>
+
+        <input type="radio" id="B4VN" name="Q4" value="VN">
+        <label for="B4VN">Disagree a lot</label><br>
+
+        <p class="question">5. How much do you agree with B5?</p>        
+
+        <input type="radio" id="B5VP" name="Q5" value="VP">
+        <label for="B5VP">Agree a lot</label><br>
+
+        <input type="radio" id="B5P" name="Q5" value="P">
+        <label for="B5P">Agree a little</label><br>   
+
+        <input type="radio" id="B5U" name="Q5" value="U">
+        <label for="B5U">Undecided</label><br>    
+
+        <input type="radio" id="B5N" name="Q5" value="N">
+        <label for="B5N">Disagree a little</label><br>
+
+        <input type="radio" id="B5VN" name="Q5" value="VN">
+        <label for="B5VN">Disagree a lot</label><br>          
+
+        <input type="submit" value="Submit" >
+
+    </form>
+
+
+</section>
+
+
+
+
+<footer>
+    <h5>&copy; 2018. All rights reserved.</h5> 
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+<script src="scripts/script.js"></script>
+</body>
+</html>
--- yourscript.php
+++ yourscript.php
@@ -1,106 +1,185 @@
-<html>
+<html lang="en"> 
+<head>
+
+	<title>The Anti-Twitter Project</title> 
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+	<meta name="description" content="This webpage explains the problem with the Attention Economy, how it effects our political landscape and some potential tech-based solutions" /> 
+	<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css" />
+
+	<link rel="stylesheet" href="css/main.css" /> 
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+</head>
+
 <body>
 
-Welcome <?php echo $_GET["name"]; ?> <br>
+	<section>
+
+		<header>
+
+			<h3>
+				The Anti-Twitter Project
+			</h3>
+
+		</header>
+	</section>
+
+	<section class="hero">
+		<div class="scrim">
+			<h2></h2>
+		</div>
+	</section>
+
+	Welcome <?php echo $_GET["name"]; ?> <br>
 <!-- Your email address is: <?php echo $_GET["email"]; ?> <br>
- -->
+-->
 
 <?php
 //Writing answers to text file
-$myfile = fopen("newfile.txt", "w+") or die("Unable to open file!");
-$x = $_GET["Q1"];
-$txt =$x . "\n";
-fwrite($myfile, $txt);
 
-$x = $_GET["Q2"];
-$txt = $x . "\n";
-fwrite($myfile, $txt);
+$profileName = $_GET["name"];
+if(strcmp($_GET["Issue"], "ProfileName")==0){
+	$myprofile = fopen($_GET["name"] . " Profile.txt", "w+") or die("Unable to open file!");
+} 
+else {
 
-$x = $_GET["Q3"];
-$txt = $x . "\n";
-fwrite($myfile, $txt);
+	$myfile = fopen("newfile.txt", "w+") or die("Unable to open file!");
 
-$x = $_GET["Q4"];
-$txt = $x . "\n";
-fwrite($myfile, $txt);
+	$x = $_GET["Q1"];
+	$txt =$x . "\n";
+	fwrite($myfile, $txt);
 
-$x = $_GET["Q5"];
-$txt = $x . "\n";
-fwrite($myfile, $txt);
+	$x = $_GET["Q2"];
+	$txt = $x . "\n";
+	fwrite($myfile, $txt);
 
-//echo file_get_contents("newfile.txt") . "<br>";
-rewind($myfile);
+	$x = $_GET["Q3"];
+	$txt = $x . "\n";
+	fwrite($myfile, $txt);
+
+	$x = $_GET["Q4"];
+	$txt = $x . "\n";
+	fwrite($myfile, $txt);
+
+	$x = $_GET["Q5"];
+	$txt = $x . "\n";
+	fwrite($myfile, $txt);
+
+	//echo file_get_contents("newfile.txt") . "<br>";
+	rewind($myfile);
 
 
-//Answer Analysis
-$Score = 0;
+	//Answer Analysis
+	$Score = 0;
 
-while(!feof($myfile)) {
-  $value = fgets($myfile);
-  if (strcmp($value, "VP\n")==0) {
-  	$Score = $Score + 3;
-  } else if (strcmp($value, "P\n")==0) {
-  	$Score = $Score + 1;
-  } else if (strcmp($value, "N\n")==0) {
-  	$Score = $Score - 1;
-  } else if (strcmp($value, "VN\n")==0) {
-  	$Score = $Score - 3;
-  }   
+	while(!feof($myfile)) {
+		$value = fgets($myfile);
+		if (strcmp($value, "VP\n")==0) {
+			$Score = $Score + 3;
+		} 
+		else if (strcmp($value, "P\n")==0) {
+			$Score = $Score + 1;
+		} 
+		else if (strcmp($value, "N\n")==0) {
+			$Score = $Score - 1;
+		} 
+		else if (strcmp($value, "VN\n")==0) {
+			$Score = $Score - 3;
+		}   
+	}
+	fclose($myfile);
+
+	//echo $_GET["Issue"] . "<br>";
+
+	//echo "A = " . $Score . "<br>";
+
+	//Creates/(adds to) a user's profile.txt 
+	$profilecheck = ($_GET["name"] . " Profile.txt");
+	$IssueDone = false;
+	if (file_exists($profilecheck)){
+	  // echo "<br>THIS FILE EXISTS<br><br>";
+		$myprofile = fopen($profilecheck, "a+") or die("Unable to open file!");
+		while(!feof($myprofile)) {
+			$IssueCheck = fgets($myprofile);
+			if (strcmp($IssueCheck, $_GET["Issue"] . "\n")==0) {
+				$IssueDone = true;
+			}
+		}
+		if ($IssueDone){
+	    // echo "<br>" . $_GET["Issue"] . " " . $IssueDone ? 'true' : 'false' . "<br>";
+		} else {
+			AddToProfile($myprofile);
+		}
+	}
+	else {
+		$myprofile = fopen($profilecheck, "w+") or die("Unable to open file!");
+		AddToProfile($myprofile);
+	}
+	echo "<br>" . file_get_contents($_GET["name"] . " Profile.txt") . "<br>";
 }
-fclose($myfile);
-
-echo $_GET["Issue"] . "<br>";
-
-//echo "A = " . $Score . "<br>";
-
-//Creates/(adds to) a user's profile.txt 
-$profilecheck = ($_GET["name"] . " Profile.txt");
-$IssueDone = false;
-if (file_exists($profilecheck)){
-  // echo "<br>THIS FILE EXISTS<br><br>";
-  $myprofile = fopen($profilecheck, "a+") or die("Unable to open file!");
-  while(!feof($myprofile)) {
-    $IssueCheck = fgets($myprofile);
-    if (strcmp($IssueCheck, $_GET["Issue"] . "\n")==0) {
-    $IssueDone = true;
-    }
-  }
-  if ($IssueDone){
-    // echo "<br>" . $_GET["Issue"] . " " . $IssueDone ? 'true' : 'false' . "<br>";
-  } else {
-    AddToProfile($myprofile);
-  }
-}
-  else {
-  $myprofile = fopen($profilecheck, "w+") or die("Unable to open file!");
-  AddToProfile($myprofile);
-}
-echo "<br>" . file_get_contents($_GET["name"] . " Profile.txt") . "<br>";
-
 
 function AddToProfile($File){
-  global $Score;
-  if ($Score==15){
-    $Stance = "You have an extremely positive Stance to issue " . $_GET["Issue"];
-  } else if ($Score>=10){
-    $Stance = "You have a very positive Stance to issue " . $_GET["Issue"];
-  } else if ($Score>=5){
-    $Stance = "You have a positive Stance to issue " . $_GET["Issue"];
-  } else if ($Score>=-4){
-    $Stance = "You have a neutral Stance to issue " . $_GET["Issue"];
-  } else if ($Score>=-9){
-    $Stance = "You have a negative Stance to issue " . $_GET["Issue"];
-  } else if ($Score>=-14){
-    $Stance = "You have a very negative Stance to issue " . $_GET["Issue"];
-  } else {
-    $Stance = "You have an extremely negative Stance to issue " . $_GET["Issue"];
-  }
-  echo "<br>" . $Stance . "<br>";
-  fwrite($File, $_GET["Issue"] . "\n" . $Score . "\n");
-  fclose($File);
+	global $Score;
+	if ($Score==15){
+		$Stance = "You have an extremely positive Stance to issue " . $_GET["Issue"];
+	} else if ($Score>=10){
+		$Stance = "You have a very positive Stance to issue " . $_GET["Issue"];
+	} else if ($Score>=5){
+		$Stance = "You have a positive Stance to issue " . $_GET["Issue"];
+	} else if ($Score>=-4){
+		$Stance = "You have a neutral Stance to issue " . $_GET["Issue"];
+	} else if ($Score>=-9){
+		$Stance = "You have a negative Stance to issue " . $_GET["Issue"];
+	} else if ($Score>=-14){
+		$Stance = "You have a very negative Stance to issue " . $_GET["Issue"];
+	} else {
+		$Stance = "You have an extremely negative Stance to issue " . $_GET["Issue"];
+	}
+	echo "<br>" . $Stance . "<br>";
+	fwrite($File, $_GET["Issue"] . "\n" . $Score . "\n");
+	fclose($File);
 }
 
 ?>
 
+<?php
+
+if (isset($_POST['QuizA']) || isset($_POST['QuizB'])) {
+    // handle textarea
+
+    if (isset($_POST['QuizA'])) {
+        header('Location: /QuizA.php');
+        exit();
+    }
+
+    header('Location: /QuizB.php');
+    exit();
+}
+
+?>
+
+<form>
+  <fieldset>
+<!--   	<input type="hidden" name="name" value = <?php$_GET["name"]?>> -->
+    <input type="submit" name="QuizA" value="Quiz A">
+    <input type="submit" name="QuizB" value="Quiz B">
+  </fieldset>
+</form>
+
+
+</section>
+
+
+
+
+<footer>
+	<h5>&copy; 2018. All rights reserved.</h5> 
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+<script src="scripts/script.js"></script>
 </body>
 </html>
